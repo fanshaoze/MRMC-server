b3e64c6cae1debdeef7b6538b6209dd1f6e1a4c1
fanshaoze <fanshaoze123123@163.com>
2019-03-13 16:36:42 +0800

    BFT使用文件测试通过，需要实际测试

--- src/Main.java
+++ src/Main.java
@@ -27,16 +27,19 @@ public class Main// 主类
 	
 	public static void main(String[] args)// 程序的入口点
 	{
+		
+/*
 		Connections.receiveList = new ArrayList<ConnectionThreadReceive>();// 初始化连接列表
 		Connections.receiveListLock = new Object();
 		Connections.sendCommandList = new ArrayList<ConnectionThreadSendCommand>();
 		Connections.sendCommandListLock = new Object();
-		
+		*/
 		rootMacaddr = "04:F0:21:39:64:26";
 		
 		nodes = new ArrayList<NodeInfo>();// 初始化节点信息列表
 		nodesLock = new Object();// 初始化列表的锁
-
+		BFSofMRMC();
+/*
 		str = new ServerThreadReceive();// 创建用于接收邻居信息的服务器线程
 		str.start();// 启动线程
 
@@ -56,6 +59,7 @@ public class Main// 主类
 				SendConfiguration();
 			}
 		}
+		*/
 	}
 
 	public static void SendConfiguration()// 下发所有节点的配置
@@ -79,8 +83,8 @@ public class Main// 主类
 		int front = 0;
 		int back = 0;
 		int elem = 0;
-		int a,b,c,d;
-		int flag = 0;
+		int a,b,c,d,e;
+		int flag,flag1 = 0;
 		NodeInfo niTemp;
 		NodeInfo niTemp1;
 		int radioNo = 0;
@@ -95,17 +99,29 @@ public class Main// 主类
 		int [][] signaled = new int [nodeNum][radioNum];
 		double[][] resultTemp = new double[nodeNum][radioNum];
 		String[] results = new String[nodeNum];
-		
+		System.out.print(radioNum+" "+nodeNum);
 		
 		for(NodeInfo ni: Main.nodes){
 			
-			if(ni.nodeID.equals(rootMacaddr)){
+			if(ni.nodeID.equalsIgnoreCase(rootMacaddr)){
 				//ni_temp = (NodeInfo)
 			}
 			//i++;
 		}
 		
 		compose();
+		for(a = 0;a<nodeNum;a++){
+			for(b = 0;b<radioNum;b++){
+				for(d = 0;d<nodeNum;d++){
+					for(e = 0;e<radioNum;e++){
+						System.out.print(NeiInforArrary[a][d][b][e]+" ");
+					}
+					System.out.print("&");
+				}
+				System.out.println(b+"b");
+			}
+			System.out.println(a+"a");
+		}
 		int que[] = new int[1000];
 		for(i = 0;i<1000;i++){
 			que[i] = -1;
@@ -133,6 +149,7 @@ public class Main// 主类
 		//队列，元素是nodeID，每次找的射频是 t%node-number，找这个neighbor下的
 		//System.out.println("课程：" + c3.name + "第一次出现的索引位置为：" + coursesToSelect.indexOf(c3));
 		while(front != back){
+			flag1 = 0;
 			String radioMac = null;
 			int channel = 0;
 			String ssid;
@@ -140,24 +157,48 @@ public class Main// 主类
 			front += 1;
 			niTemp = nodes.get(elem);
 			for (i = 0;i<radioNum;i++){
-				if (visited[elem][i] == 0) {
+				if (visited[elem][i] == 0 && niTemp.radioInfo.get(i).neighborList.size()>0) {
+					//邻居判断
 					visited[elem][i] = 1;
 					radioMac = niTemp.radioInfo.get(i).radioNumber;
 					if(i == 0) channel = channel2G;
 					else channel = channel5G;
+					flag1 = 1;
 					break;
 				}
+				else if (visited[elem][i] == 0 && niTemp.radioInfo.get(i).neighborList.size()== 0) {
+					//邻居判断
+					visited[elem][i] = 1;
+					continue;
+				}
+			}
+			if (flag1 == 0){
+				continue;
 			}
 			flag = 0;
 			String[] submac = radioMac.split(":");
 			ssid = "link" + submac[submac.length-1];
+			System.out.println(ssid);
 			radioTemp = niTemp.radioInfo.get(i);
+			for(a = 0;a<nodeNum;a++){
+				for(b = 0;b<radioNum;b++){
+					for(d = 0;d<nodeNum;d++){
+						for(e = 0;e<radioNum;e++){
+							System.out.print(NeiInforArrary[a][d][b][e]+" ");
+						}
+						System.out.print("&");
+					}
+					System.out.println(b+"b");
+				}
+				System.out.println(a+"a");
+			}
 			for(NeighborInfo nei : radioTemp.neighborList){
 				for(j = 0;j<nodeNum;j++){
 					niTemp1 = nodes.get(j);
-					for(k = 0;k<radioNo;k++){
+					for(k = 0;k<radioNum;k++){
 						radioTemp1 = niTemp1.radioInfo.get(k);
-						if(radioTemp1.radioNumber.equals(nei.neighborMac)){
+						System.out.println(radioTemp1.radioNumber+" "+nei.neighborMac);
+						if(radioTemp1.radioNumber.equalsIgnoreCase(nei.neighborMac)){
 							if(visited[j][k] == 0){
 								flag = 1;
 								//应当增加关于信噪比的判断
@@ -197,13 +238,29 @@ public class Main// 主类
 		}
 		for(a = 0;a<nodeNum;a++){
 			for(b = 0;b<radioNum;b++){
+				for(d = 0;d<nodeNum;d++){
+					for(e = 0;e<radioNum;e++){
+						System.out.print(NeiInforArrary[a][d][b][e]+" ");
+					}
+					System.out.print("&");
+				}
+				System.out.println(b+"b");
+			}
+			System.out.println(a+"a");
+		}
+		for(a = 0;a<nodeNum;a++){
+			for(b = 0;b<radioNum;b++){
 				if(signaled[a][b] ==1){
 					for(c = 0;c<nodeNum;c++){
 						for(d = 0;d<radioNum;d++){
-							if(NeiInforArrary[c][a][d][b]>-70 && NeiInforArrary[a][c][b][d]>-70 && 
-									nodes.get(c).radioInfo.get(b).direction == "down"){
+							if(NeiInforArrary[c][a][d][b]>-70.0 && NeiInforArrary[a][c][b][d]>-70.0 && 
+									NeiInforArrary[c][a][d][b]<0.0 && NeiInforArrary[a][c][b][d]<0.0 &&
+									nodes.get(c).radioInfo.get(d).direction == "down"){
+								System.out.println(NeiInforArrary[c][a][d][b]+" %% "+NeiInforArrary[a][c][b][d]);
 								nodes.get(a).radioInfo.get(b).assignedssid = nodes.get(c).radioInfo.get(d).assignedssid;
 								nodes.get(a).radioInfo.get(b).assignedChannel = nodes.get(c).radioInfo.get(d).assignedChannel;
+								System.out.println(" && ");
+								System.out.println(nodes.get(a).radioInfo.get(b).radioNumber+" "+nodes.get(c).radioInfo.get(d).radioNumber);
 								//nodes.get(a).radioInfo.get(b).direction = "up";
 								//还不是最优的，
 
@@ -216,13 +273,24 @@ public class Main// 主类
 		for(a = 0;a<nodeNum;a++){
 			results[a] = "SETLINK ";
 			for(b = 0;b<radioNum;b++){
-				String subRadioMac = nodes.get(elem).radioInfo.get(i).assignedssid.substring(0, 4);
+				//String subRadioMac = nodes.get(elem).radioInfo.get(i).assignedssid.substring(0, 4);
 				if (resultTemp[a][b] == 1){
-					radioTemp1 =  nodes.get(elem).radioInfo.get(i);
+					radioTemp1 =  nodes.get(a).radioInfo.get(b);
 					results[a] += radioTemp1.radioNumber+"#"+radioTemp1.assignedChannel+"#"+radioTemp1.assignedssid+" ";
+					System.out.println(results[a]);
+				}
+				else{
+					radioTemp1 =  nodes.get(a).radioInfo.get(b);
+					results[a] += radioTemp1.radioNumber+"#"+"DISABLED"+" ";
+					System.out.println(results[a]);
 				}
 			}
-			results[a] = results[a].substring(0, results[a].length()-2);
+			results[a] = results[a].substring(0, results[a].length()-1);
+			System.out.println(results[a]);
+		}
+		System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+		for(a = 0;a<nodeNum;a++){
+			System.out.println(results[a]);
 		}
 		for(a = 0;a<nodeNum;a++){
 			SendToNode(nodes.get(a).nodeID, results[a]);
@@ -263,6 +331,7 @@ public class Main// 主类
 		}
 		for(a = 0;a<nodes.size();a++){
 			ni = nodes.get(a);
+			System.out.println(ni.nodeID);
 			for(b = 0;b<ni.radioInfo.size();b++){
 				ra = ni.radioInfo.get(b);
 				neiNum = ra.neighborList.size(); 
@@ -272,7 +341,9 @@ public class Main// 主类
 						nis = nodes.get(d);
 						for(e = 0;e<nis.radioInfo.size();e++){
 							ras = nis.radioInfo.get(e);
-							if(nei.neighborMac.equals(ras.radioNumber)){
+							System.out.println(nei.neighborMac+" "+ras.radioNumber);
+							System.out.println(nei.neighborMac.equalsIgnoreCase(ras.radioNumber));
+							if(nei.neighborMac.equalsIgnoreCase(ras.radioNumber)){
 								NeiInforArrary[a][d][b][e] = nei.signal;
 							}
 						}	
@@ -306,17 +377,36 @@ public class Main// 主类
 		System.out.println("send here!");
 		ctsc.sendCommand(command);// 发送命令
 	}
-	public static void BFStestinit(String neighborinform) throws IOException // 线程运行时，执行此函数
+	public static void BFStestinit(String neighborinform)  // 线程运行时，执行此函数
 	{
+		int j = 0;
+		String lines[] = new String[8];
+		/*
+		lines[0] = "NEIGHBOR 04:F0:21:39:64:12 04:F0:21:39:64:11 36 04:f0:21:39:64:1B#-28#-95#780.000000#VHT-MCS$9$80MHz$VHT-NSS$2#866.000000#VHT-MCS$9$80MHz$short$GI$VHT-NSS$2 04:f0:21:39:64:17#-28#-95#780.000000#VHT-MCS$9$80MHz$VHT-NSS$2#866.000000#VHT-MCS$9$80MHz$short$GI$VHT-NSS$2";
+		lines[1] = "NEIGHBOR 04:F0:21:39:64:1C 04:F0:21:39:64:1B 36 04:f0:21:39:64:11#-28#-95#780.000000#VHT-MCS$9$80MHz$VHT-NSS$2#866.000000#VHT-MCS$9$80MHz$short$GI$VHT-NSS$2 04:f0:21:39:64:17#-28#-95#780.000000#VHT-MCS$9$80MHz$VHT-NSS$2#866.000000#VHT-MCS$9$80MHz$short$GI$VHT-NSS$2";
+		lines[2] = "NEIGHBOR 04:F0:21:39:64:1C 04:F0:21:39:64:1C 11 04:f0:21:39:64:14#-71#-95#780.000000#VHT-MCS$9$80MHz$VHT-NSS$2#866.000000#VHT-MCS$9$80MHz$short$GI$VHT-NSS$2 04:f0:21:39:64:16#-28#-95#781.000000#VHT-MCS$9$80MHz$VHT-NSS$2#867.000000#VHT-MCS$9$80MHz$short$GI$VHT-NSS$2";
+		lines[3] = "NEIGHBOR 04:F0:21:39:64:18 04:F0:21:39:64:17 36 04:f0:21:39:64:11#-28#-95#780.000000#VHT-MCS$9$80MHz$VHT-NSS$2#866.000000#VHT-MCS$9$80MHz$short$GI$VHT-NSS$2";
+		lines[4] = "NEIGHBOR 04:F0:21:39:64:18 04:F0:21:39:64:18 11 04:f0:21:39:64:20#-28#-95#780.000000#VHT-MCS$9$80MHz$VHT-NSS$2#866.000000#VHT-MCS$9$80MHz$short$GI$VHT-NSS$2 04:f0:21:39:64:14#-28#-95#780.000000#VHT-MCS$9$80MHz$VHT-NSS$2#866.000000#VHT-MCS$9$80MHz$short$GI$VHT-NSS$2";
+		lines[5] = "NEIGHBOR 04:F0:21:39:64:14 04:F0:21:39:64:14 11 04:f0:21:39:64:1C#-71#-95#780.000000#VHT-MCS$9$80MHz$VHT-NSS$2#866.000000#VHT-MCS$9$80MHz$short$GI$VHT-NSS$2 04:f0:21:39:64:18#-28#-95#780.000000#VHT-MCS$9$80MHz$VHT-NSS$2#866.000000#VHT-MCS$9$80MHz$short$GI$VHT-NSS$2";
+		lines[6] = "NEIGHBOR 04:F0:21:39:64:16 04:F0:21:39:64:16 11 04:f0:21:39:64:1C#-28#-95#780.000000#VHT-MCS$9$80MHz$VHT-NSS$2#866.000000#VHT-MCS$9$80MHz$short$GI$VHT-NSS$2";
+		lines[7] = "NEIGHBOR 04:F0:21:39:64:20 04:F0:21:39:64:20 11 04:f0:21:39:64:18#-28#-95#780.000000#VHT-MCS$9$80MHz$VHT-NSS$2#866.000000#VHT-MCS$9$80MHz$short$GI$VHT-NSS$2";
+		*/
 		String str1 = "temp.txt"; 
 		try {
 			FileReader fr = new FileReader(str1);
 			BufferedReader br = new BufferedReader(fr);   
-			neighborinform = br.readLine();   
+			try {
+				neighborinform = br.readLine();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}   
 		
 		
 			InputStream is;
 			Scanner scanner = null;
+			String line = neighborinform;
+			//neighborinform = lines[0];
 		//is = neighborinform;
 		//is = connection.getInputStream(); // 获取TCP连接的输入流
 		//System.out.print("aaaa\n");
@@ -324,9 +414,11 @@ public class Main// 主类
 		//scanner = new Scanner(is); // 用Scanner包装输入流
 		//while (true) // 一直循环，每次读取一行
 			while (neighborinform != null) {
-			{
-				String line = neighborinform;
-				System.out.print(line);
+			
+				line = neighborinform;
+				System.out.println(line);
+				
+				//System.out.print(line);
 				line.replace("\r", ""); // 去除行尾的换行符
 				line.replace("\n", "");
 				//line = "NEIGHBOR 04:F0:21:39:64:1C 04:F0:21:39:64:1B 36 04:f0:21:39:64:1f"
@@ -336,100 +428,111 @@ public class Main// 主类
 				System.out.println("command : "+command);
 				if (command.equals("NEIGHBOR")) // 如果这一行是邻居信息
 				{
-					synchronized (Main.nodesLock)
+
+					boolean found = false;
+					NodeInfo foundni = null;
+					System.out.println(Main.nodes.size());
+					//打印信息
+					for (NodeInfo ni : Main.nodes)// 在节点列表中查找这个节点
 					{
-						boolean found = false;
-						NodeInfo foundni = null;
-						//打印信息
-						for (NodeInfo ni : Main.nodes)// 在节点列表中查找这个节点
+						if (ni.nodeID.equals(parts[1]))// 如果找到，则跳出循环
 						{
-							if (ni.nodeID.equals(parts[1]))// 如果找到，则跳出循环
+							found = true;
+							foundni = ni;
+							break;
+						}
+					}
+					if (found == false) // 如果没有找到，则创建一个，并添加到节点列表中
+					{
+						foundni = new NodeInfo();
+						foundni.nodeID = parts[1];
+						System.out.println("foundni.nodeID : "+foundni.nodeID);
+						foundni.radioInfo = new ArrayList<RadioInfo>();
+						Main.nodes.add(foundni);
+					}
+
+					found = false;
+					RadioInfo foundri = null;
+					for (RadioInfo ri : foundni.radioInfo)// 查找某个节点的某个radio
+					{
+						if ((ri.radioNumber.equals(parts[2]))
+								&& (ri.assignedChannel == Integer.decode(parts[3]).intValue()))// 如果找到（需要radio的mac地址和信道号都匹配）
+						{
+							found = true;
+							foundri = ri;
+						}
+					}
+					if (found == false)// 如果没有找到，则创建一个，并添加到radio列表中
+					{
+						foundri = new RadioInfo();
+						foundri.radioNumber = parts[2];
+						System.out.println("foundri.radioNumber : "+foundri.radioNumber);
+						foundri.assignedChannel = Integer.decode(parts[3]).intValue();
+						foundri.neighborList = new ArrayList<NeighborInfo>();
+						foundni.radioInfo.add(foundri);
+						//打印信息
+					}
+
+					for (int i = 4; i < parts.length; i++)// 处理邻居信息的剩余部分
+					{
+						String s = parts[i];
+						if(s.equals("NONEIGHBOR")){
+							break;
+						}
+						//打印信息
+						String[] parts2 = s.split("#");// 将邻居信息以井号为分隔符分开
+						found = false;
+						NeighborInfo foundni2 = null;
+						//打印信息
+						for (NeighborInfo ni : foundri.neighborList)// 在现有的邻居列表中查找这个邻居
+						{
+							if (ni.neighborMac.equals(parts2[0]))// 如果找到，则跳出循环
 							{
 								found = true;
-								foundni = ni;
+								foundni2 = ni;
 								break;
 							}
 						}
-						if (found == false) // 如果没有找到，则创建一个，并添加到节点列表中
+						if (found == false)// 如果没有找到，则新创建一个邻居信息项目
 						{
-							foundni = new NodeInfo();
-							foundni.nodeID = parts[1];
-							System.out.println("foundni.nodeID : "+foundni.nodeID);
-							foundni.radioInfo = new ArrayList<RadioInfo>();
-							Main.nodes.add(foundni);
+							foundni2 = new NeighborInfo();
+							foundni2.neighborMac = parts2[0];
+							System.out.println("parts2[0] : "+parts2[0]);
+							foundri.neighborList.add(foundni2);
 						}
-	
-						found = false;
-						RadioInfo foundri = null;
-						for (RadioInfo ri : foundni.radioInfo)// 查找某个节点的某个radio
-						{
-							if ((ri.radioNumber.equals(parts[2]))
-									&& (ri.assignedChannel == Integer.decode(parts[3]).intValue()))// 如果找到（需要radio的mac地址和信道号都匹配）
-							{
-								found = true;
-								foundri = ri;
-							}
+						for(int t = 0;t<7;t++){
+							System.out.println("t:"+t+" "+parts2[t]);
 						}
-						if (found == false)// 如果没有找到，则创建一个，并添加到radio列表中
-						{
-							foundri = new RadioInfo();
-							foundri.radioNumber = parts[2];
-							System.out.println("foundri.radioNumber : "+foundri.radioNumber);
-							foundri.assignedChannel = Integer.decode(parts[3]).intValue();
-							foundri.neighborList = new ArrayList<NeighborInfo>();
-							foundni.radioInfo.add(foundri);
-							//打印信息
-						}
-	
-						for (int i = 4; i < parts.length; i++)// 处理邻居信息的剩余部分
-						{
-							String s = parts[i];
-							//打印信息
-							String[] parts2 = s.split("#");// 将邻居信息以井号为分隔符分开
-							found = false;
-							NeighborInfo foundni2 = null;
-							//打印信息
-							for (NeighborInfo ni : foundri.neighborList)// 在现有的邻居列表中查找这个邻居
-							{
-								if (ni.neighborMac.equals(parts2[0]))// 如果找到，则跳出循环
-								{
-									found = true;
-									foundni2 = ni;
-									break;
-								}
-							}
-							if (found == false)// 如果没有找到，则新创建一个邻居信息项目
-							{
-								foundni2 = new NeighborInfo();
-								foundni2.neighborMac = parts2[0];
-								System.out.println("parts2[0] : "+parts2[0]);
-								foundri.neighborList.add(foundni2);
-							}
-							for(int t = 0;t<7;t++){
-								System.out.println("t:"+t+" "+parts2[t]);
-							}
-							
-							
-							foundni2.signal = Double.parseDouble(parts2[1]);
-							foundni2.noise = Double.parseDouble(parts2[2]);
-							foundni2.tx_rate = Double.parseDouble(parts2[3]);
-							foundni2.tx_QAM = parts2[4];
-							foundni2.rx_rate = Double.parseDouble(parts2[5]);
-							foundni2.rx_QAM = parts2[6];
-							foundni2.rate = 0.5*foundni2.tx_rate;// 存储邻居的信息
-							System.out.println("foundni2 : r:"+foundni2.rate+" s:"+foundni2.signal +" n:"+foundni2.noise +" t:"+
-							foundni2.tx_rate +" tq:"+foundni2.tx_QAM +" r:"+foundni2.rx_rate +" rq:"+foundni2.rx_QAM );
-							//打印信息
-						}
+						
+						
+						foundni2.signal = Double.parseDouble(parts2[1]);
+						foundni2.noise = Double.parseDouble(parts2[2]);
+						foundni2.tx_rate = Double.parseDouble(parts2[3]);
+						foundni2.tx_QAM = parts2[4];
+						foundni2.rx_rate = Double.parseDouble(parts2[5]);
+						foundni2.rx_QAM = parts2[6];
+						foundni2.rate = 0.5*foundni2.tx_rate;// 存储邻居的信息
+						System.out.println("foundni2 : r:"+foundni2.rate+" s:"+foundni2.signal +" n:"+foundni2.noise +" t:"+
+						foundni2.tx_rate +" tq:"+foundni2.tx_QAM +" r:"+foundni2.rx_rate +" rq:"+foundni2.rx_QAM );
+						//打印信息
 					}
 				}
-				neighborinform = br.readLine();  
+				j++;
+				try {
+					neighborinform = br.readLine();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
 			}
-		}
-	} catch (FileNotFoundException e) {
+			
+		
+		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
-		e.printStackTrace();
-	} 
+			e.printStackTrace();
+		} 
+	}
 }
 
 class Connections// 连接的列表
